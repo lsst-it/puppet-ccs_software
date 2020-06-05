cdb08a48556525a73520377628fc10e8314621b9
Joshua Hoblitt <josh@hoblitt.com>
2020-06-05 10:07:19 -0700

    add ccs scripts dir to default shell path

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -108,6 +108,14 @@ describe 'ccs_software class' do
         it { is_expected.to be_mode '755' } # serverspec does not like a leading 0
       end
     end
+
+    describe file('/etc/profile.d/ccs-scripts.sh') do
+      it { is_expected.to be_file }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_mode '644' } # serverspec does not like a leading 0
+      its(:content) { is_expected.to match "export PATH=${PATH}:#{basedir}/ccsadm/scripts" }
+    end
   end
 
   context 'with installations' do
