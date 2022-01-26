fefe8065bec43213a6dae70b920181dce12149d1
Joshua Hoblitt <josh@hoblitt.com>
2022-01-26 10:43:04 -0700

    fix rspec facts

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -3,24 +3,29 @@
 require 'spec_helper'
 
 describe 'ccs_software' do
-  let(:facts) { { hostname: 'foo' } }
   let(:node_params) { { 'site' => 'ls' } }
 
-  describe 'without any parameters' do
-    it { is_expected.to compile.with_all_deps }
+  on_supported_os.each do |_os, facts|
+    let(:facts) do
+      facts
+    end
 
-    %w[
-      logging.properties
-      ccsGlobal.properties
-      udp_ccs.properties
-    ].each do |f|
-      it do
-        is_expected.to contain_file("/etc/ccs/#{f}").with(
-          ensure: 'file',
-          owner: 'ccsadm',
-          group: 'ccsadm',
-          mode: '0664',
-        )
+    describe 'without any parameters' do
+      it { is_expected.to compile.with_all_deps }
+
+      %w[
+        logging.properties
+        ccsGlobal.properties
+        udp_ccs.properties
+      ].each do |f|
+        it do
+          is_expected.to contain_file("/etc/ccs/#{f}").with(
+            ensure: 'file',
+            owner: 'ccsadm',
+            group: 'ccsadm',
+            mode: '0664',
+          )
+        end
       end
     end
   end
--- spec/classes/log_spec.rb
+++ spec/classes/log_spec.rb
@@ -3,20 +3,24 @@
 require 'spec_helper'
 
 describe 'ccs_software' do
-  let(:facts) { { hostname: 'foo' } }
   let(:node_params) { { 'site' => 'ls' } }
 
-  describe 'logs' do
-    it { is_expected.to compile.with_all_deps }
+  on_supported_os.each do |_os, facts|
+    let(:facts) do
+      facts
+    end
+    describe 'logs' do
+      it { is_expected.to compile.with_all_deps }
 
-    it do
-      is_expected.to contain_file('/etc/cron.daily/ccs-log-compress').with(
-        ensure: 'file',
-        mode: '0755',
-        owner: 'root',
-        group: 'root',
-        content: %r{archive_flag=n},
-      )
+      it do
+        is_expected.to contain_file('/etc/cron.daily/ccs-log-compress').with(
+          ensure: 'file',
+          mode: '0755',
+          owner: 'root',
+          group: 'root',
+          content: %r{archive_flag=n},
+        )
+      end
     end
   end
 end
