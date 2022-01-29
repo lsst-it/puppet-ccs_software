a23db1cdeb03608491c5d9fe9d183a4ede5055f7
Joshua Hoblitt <josh@hoblitt.com>
2022-01-28 17:34:33 -0700

    fix acceptance test mod dep installation

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -9,6 +9,18 @@ configure_beaker do |host|
   # XXX this is a kludge!  We need to overwrite the first automatic
   # installation of the dev module as the `log` dir is being filtered out.
   install_module(ignore_list: ['.bundle', 'bundle', '.vendor', 'vendor'])
+  clone_git_repo_on(host, '/etc/puppetlabs/code/environments/production/modules',
+                    name: 'java_artisanal',
+                    path: 'https://github.com/lsst-it/puppet-java_artisanal',
+                    rev: 'master')
+  # java_artisanal deps
+  {
+    'puppetlabs/accounts' => '> 1',
+    'puppet/yum' => '>= 4 < 6',
+    'puppet/alternatives' => '> 3 < 5',
+  }.each do |mod, rev|
+    install_module_from_forge_on(host, mod, rev)
+  end
 end
 
 shared_examples 'an idempotent resource' do
