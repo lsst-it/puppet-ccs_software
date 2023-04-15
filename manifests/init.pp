ba5d44fddb4693b5c12f520c327c0cfbecaa9a28
Glenn Morris <rgm@stanford.edu>
2023-04-14 18:36:03 -0700

    (init) fix commentary typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,7 +27,7 @@
 # @option services [String] name-of-alias
 #   [Array] of service names/service executables (links under /opt/lsst/<alias>/bin/)
 #   Alternatively, array element can also be a hash of the form
-#   { name: "h2db, key: "value", ... }
+#   { name: "h2db", key: "value", ... }
 #   Allowed keys: cmd, user, group, workdir, env.
 #   Values specify the associated service file values.
 #
