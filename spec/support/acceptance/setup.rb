e8fc05eec7e27684b44960afdbb7c78559f7cbd7
Joshua Hoblitt <josh@hoblitt.com>
2023-06-22 09:13:08 -0700

    allow lsst/java_artisanal >= 3 for acceptance tests

--- spec/support/acceptance/setup.rb
+++ spec/support/acceptance/setup.rb
@@ -2,8 +2,8 @@
 
 configure_beaker do |host|
   install_module_from_forge_on(host, 'puppetlabs/accounts', '> 7 < 8')
+  install_module_from_forge_on(host, 'lsst/java_artisanal', '>= 2.4.0')
   install_module_from_forge_on(host, 'puppet/alternatives', '>= 4.1.0')
-  install_module_from_forge_on(host, 'lsst/java_artisanal', '>= 2.4.0 < 3')
   scp_to(host, "#{__dir__}/../../fixtures/facts/site.yaml", '/opt/puppetlabs/facter/facts.d/site.yaml')
   # XXX this is a kludge!  We need to overwrite the first automatic
   # installation of the dev module as the `log` dir is being filtered out.
