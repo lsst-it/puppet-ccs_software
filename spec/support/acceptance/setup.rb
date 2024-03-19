b947753b6cbdb61cfc397f4d2740f8eaabcd9e05
Joshua Hoblitt <josh@hoblitt.com>
2024-03-19 11:11:58 -0700

    rm acceptance test usage of puppetlabs/accounts mod
    because the accpetance tests are failing under puppet8 because of this
    bug: https://github.com/puppetlabs/puppetlabs-accounts/pull/476

--- spec/support/acceptance/setup.rb
+++ spec/support/acceptance/setup.rb
@@ -3,6 +3,5 @@
 configure_beaker(modules: :metadata) do |host|
   scp_to(host, "#{__dir__}/../../fixtures/facts/site.yaml", '/opt/puppetlabs/facter/facts.d/site.yaml')
   install_puppet_module_via_pmt_on(host, 'puppet/alternatives', '5')
-  install_puppet_module_via_pmt_on(host, 'puppetlabs/accounts', '8')
   install_puppet_module_via_pmt_on(host, 'puppetlabs/java', '10')
 end
